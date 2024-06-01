7b9f06276622e773fa0bcedd3ed7a18a2baf24a4
lutfiHidayanto <hajidlutfi@gmail.com>
2024-06-01 14:19:15 +0700

    CBAM Refactor

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -49,7 +49,8 @@ from ultralytics.nn.modules import (
     Segment,
     Silence,
     WorldDetect,
-    ResBlock_CBAM
+    ResBlock_CBAM,
+    CBAM
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -886,7 +887,8 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             DWConvTranspose2d,
             C3x,
             RepC3,
-            ResBlock_CBAM
+            ResBlock_CBAM,
+            CBAM
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
